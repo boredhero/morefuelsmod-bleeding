0044498fbaa66813969dbe88534769e66e5fa3ff
Noah Martino <treeclan12@gmail.com>
2016-08-29 20:31:46 -0400

    Removed Bitumen and all recipies for Bitumen from 1.4.4

--- build/sources/main/java/com/bored/morefuelsmod/Main.java
+++ build/sources/main/java/com/bored/morefuelsmod/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
 	public static final String modid = "morefuelsmod-bleeding";
 	public static final String name = "More Fuels Mod";
-	public static final String version = "1.5.alpha.4";
+	public static final String version = "1.5.beta.1";
 
 	@Mod.Instance(modid)
 	public static Main instance;
